13500cb770c87b4b2a1ba302f34c2658b2f220cc
oliverDOTnet <amdcorei7>
2015-02-22 13:44:16 +0100

    Test3

--- Evaluant.Calculator.Play/Program.cs
+++ Evaluant.Calculator.Play/Program.cs
@@ -19,7 +19,7 @@ namespace NCalc.Play
 				"false || not (false and true)",
 				"3 > 2 and 1 <= (3-2)",
 				"3 % 2 != 10 % 3",
-                "3+2"
+                "3+1"
                 //"if( [age] >= 18, 'majeur', 'mineur')",
                 //"CalculateBenefits([user]) * [Taxes]"
 			};
